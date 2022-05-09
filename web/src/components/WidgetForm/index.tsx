ea47d242a280d9986b36cf52c660a181e8003b00
victor-luna <victor.vazguimaraes@gmail.com>
2022-05-09 12:16:14 -0300

    Aula 03 - BackEnd

--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -3,7 +3,6 @@ import { CloseButton } from "../CloseButton";
 import bugImageUrl from "../../assets/bug.svg";
 import ideaImageUrl from "../../assets/idea.svg";
 import thoughtImageUrl from "../../assets/thought.svg";
-
 import { useState } from "react";
 import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
 import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
